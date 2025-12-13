766bbb29dac1abd778e4755299e2b272fbdc0e27
David Medek <d4vid.medek@gmail.com>
2025-12-13 15:04:25 +0100

    add API objects:  User, Bank account

---
+++ src/api/tegb/bank_account_api.ts
@@ -0,0 +1,25 @@
+import { APIRequestContext } from "@playwright/test";
+
+export class BankAccountApi {
+  readonly request: APIRequestContext;
+  readonly apiUrl = "https://tegb-backend-877a0b063d29.herokuapp.com/tegb";
+
+  constructor(request: APIRequestContext) {
+    this.request = request;
+  }
+
+  async createBankAccount(startBalance: number, type: string, token: string) {
+    const response = await this.request.post(this.apiUrl + "/accounts/create", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        startBalance,
+        type,
+      },
+    });
+
+    return response;
+  }
+}
